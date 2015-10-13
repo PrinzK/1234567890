ea3f93f501f22de7d9dcecefae2429297d8d6c66
Krish <sreekrishnapandi@gmail.com>
2015-10-13 13:55:23 +0200

    Cleanup n Refactor

--- linefollowKrishna.py
+++ linefollowKrishna.py
@@ -1,4 +1,4 @@
-import pi2go
+from pi2go_library import pi2go
 import time
 import threading
 
@@ -29,22 +29,17 @@ def line_follower():
     STOP = False
     while True:
         #print "line follower %f" %time.time()
-        # we don't need to start a thread if theres only one.....
         # print 'get dist: %f' % time.time()
         left = pi2go.irLeftLine()
         right = pi2go.irRightLine()
         # print 'get ir: %f' % time.time()
         if not left and not right:    # If both sensors are the on --> forward
-            # pi2go.forward(speed)
             STATE = 00
         elif left and not right:          # If the left sensor is Off --> move right
-            # pi2go.turnForward(speed+change, speed-change)
             STATE = 10
         elif right and not left:         # If the right sensor is off --> move left
-            # pi2go.turnForward(speed - change, speed + change)
             STATE = 01
-        else:                   # stop
-            # pi2go.stop()
+        else:
             STATE = 11
 
         if time.time() - start > 0.15:
@@ -57,18 +52,15 @@ def line_follower():
                 time.sleep(0.15)
             start = time.time()
 
-
         if STATE == prev_STATE:
             pass
         elif STATE == 00:
             pi2go.forward(speed)
-            # pi2go.stop()
         elif STATE == 10:
             pi2go.turnForward(speed + change, speed - change)
         elif STATE == 01:
             pi2go.turnForward(speed - change, speed + change)
         elif STATE == 11:
-            # pi2go.forward(speed)
             pi2go.stop()
 
         prev_STATE = STATE
@@ -76,20 +68,12 @@ def line_follower():
 if __name__ == "__main__":
     pi2go.init()
     pi2go.cleanup()
-
-    run_event = threading.Event()
-    run_event.set()  
-        
     pi2go.init()
 
     try:
         line_follower()
-            
     except KeyboardInterrupt:
-        run_event.clear()        
-        #t1.join()
-      #  t2.join()
         print 'end'
     
-    finally: # Even if there was an error, cleanup
+    finally:
         pi2go.cleanup()
