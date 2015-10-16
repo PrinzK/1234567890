57862d5dc75bb1f9aae75fcca5ce6e8b46e4cfed
PrinzK <david_nophut@hotmail.com>
2015-10-16 11:32:07 +0200

    ups!

--- distance_and_broadcast.py
+++ distance_and_broadcast.py
@@ -1,22 +1,28 @@
 import pi2go, time
 import communication
+from __future__ import print_function
 
 DIST_RUN = 40
 DIST_STOP = 15
+TIME_BETWEEN_MEASUREMENT = 0.2
 start = time.time()
 dist = 50
 state = "RUN"
 prev_state = state
 
 pi2go.init()
+pi2go.cleanup()
+pi2go.init()
 
 LEDon = 1024
 LEDoff = 0
+log = open(senseless_log)
 
 try:
 	while True:
-		if time.time() - start > 0.15:
-			print "Starting distance measuring"
+		if time.time() - start > TIME_BETWEEN_MEASUREMENT:
+			print("Starting distance measuring", file = log)
+			#dist = 5+4
 			dist = (int(pi2go.getDistance()*10))/10.0
 			start = time.time()
 			#print "Distance measuring finished"
