5e7e9f81c5fd548db5eec07eac3eb16eb95b8d21
PrinzK <david_nophut@hotmail.com>
2015-10-27 18:30:32 +0100

    more untested control

--- Control.py
+++ Control.py
@@ -1,15 +1,15 @@
 import communication
+
 COMMAND_PORT = 5005
 MODE_PORT= 5006 
 SENDING_ATTEMPTS = 20
 
 sock = communication.init_nonblocking_receiver('', MODE_PORT)
-state_list = []
-for x in range (0,12):
-    state_list.append("RUN")
-
 
 def get_modes():
+    state_list = []
+    for x in range (0,15):
+        state_list.append("RUN")
     message_list = communication.receive_message_list(sock)
     state_list = communication.update_state_list(state_list, message_list)
     print state_list
@@ -23,3 +23,5 @@ def start_all_bots():
     communication.send_x_broadcast_messages(COMMAND_PORT, "STARTALL", SENDING_ATTEMPTS)
 def stop_all_robots():
     communication.send_x_broadcast_messages(COMMAND_PORT, "GOIDLE", SENDING_ATTEMPTS)
+    
+get_modes()
