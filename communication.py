6f1d7b92295e12828c33fcec1f55be2deb87d7cf
Krish <sreekrishnapandi@gmail.com>
2015-10-29 00:20:21 +0100

    Cleanup + refactor

--- communication.py
+++ communication.py
@@ -17,6 +17,7 @@ def send_broadcast_message(port, message):
 	finally:
 		sock.close()
 
+
 def send_udp_unicast_message(address, port, message):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	try:
@@ -27,65 +28,57 @@ def send_udp_unicast_message(address, port, message):
 	finally:
 		sock.close()
 
+
 def init_receiver(address, port):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock.bind((address, port))
 	return sock
-	
+
+
 def init_nonblocking_receiver(address, port):
 	sock = init_receiver(address, port)
 	sock.setblocking(0)
 	return sock
-	
+
+
 def init_blocking_receiver(address, port, timeout):
 	sock = init_receiver(address, port)
 	sock.settimeout(timeout)
 	return sock
-	
+
+
 def receive_message(sock):
 	data = ""
 	addr = ""
 	try:
 		data, addr = sock.recvfrom(1024)
-		#print "received message: " +  data + " at: %f" %time.time()
+		# print "received message: " +  data + " at: %f" %time.time()
 	except socket.error as e:
 		#print e
 		pass
 	finally:
 		return (data, addr)
-  
+
+
 def receive_message_list(sock):
-    message_list = []
-    cur_data, cur_addr = receive_message(sock)
-    while cur_data != '':        
-        message_list.append((cur_data, cut_addr_to_id(cur_addr)))
-        cur_data, cur_addr = receive_message(sock)
-    return message_list 
-"""    
-def premodify_message_list(message_list):
-    for i in message_list:
-        i = ()
-    return message_list
-"""    
+	message_list = []
+	cur_data, cur_addr = receive_message(sock)
+	while cur_data != '':
+		message_list.append((cur_data, cut_addr_to_id(cur_addr)))
+		cur_data, cur_addr = receive_message(sock)
+	return message_list
+
+
 def cut_addr_to_id((addr, port)):
-    ID = addr.split()[0][len(addr)-2:len(addr)-0]
-    return int(ID)
+	ID = addr.split()[0][len(addr)-2:len(addr)-0]
+	return int(ID)
 		
+
 def get_id():
 	return int(commands.getoutput("/sbin/ifconfig").split("\n")[16].split()[1][18:])
 
+
 def get_ip():
 	return commands.getoutput("/sbin/ifconfig").split("\n")[16].split()[1][5:]
- 
-def update_state_list(state_list, message_list, OWN_ID):
-    for message, ID in message_list:
-        if ID == OWN_ID:
-            continue
-        print message
-        if message == "STOP":
-            message = False
-        elif message == "RUN" or message == "SLOW":
-            message = True
-        
-        state_list[ID] = message
-    return state_list
+
+
--- ygo.py
+++ ygo.py
@@ -6,8 +6,8 @@ import time
 PORT= 5005
 LED_ON = 1000
 LED_OFF = 0
-RUNNING = True
-STOPPED = False
+CLEAR = True
+OBSTACLE = False
 
 # Configuration
 MIN_DIST= 15
@@ -19,13 +19,16 @@ R_SPEED_SLOW = 30
 
 OWN_ID = communication.get_id()
 
-state = "IDLE"
+state = "NOT_IDLE"
 sub_state_motor = "RUN"
 prev_sub_state_motor = "STOP"
 sub_sub_state_stop = "OWN"
 state_list = []
+prev_state_list = []
+
 for x in range (0,12):
-    state_list.append(RUNNING)
+    state_list.append(CLEAR)
+    prev_state_list.append(CLEAR)
 slow = False
 
 #distance = 50
@@ -34,13 +37,27 @@ last_dist_check = 0
 try:
     pi2go.init()
     sock = communication.init_nonblocking_receiver('', PORT)
+
+    def update_state_list():
+        for msg, ID in message_list:
+            if ID == OWN_ID:
+                continue
+            # print OWN_ID, " : ", message, " from ", ID
+            if msg == "False":
+                msg = False
+            elif message == "True" or message == "SLOW":
+                msg = True
+            state_list[ID] = msg
+        # return state_list
+
     while True:
         if state == "IDLE":
             pi2go.setAllLEDs(LED_OFF, LED_OFF, LED_ON)
             message, addr = communication.receive_message(sock)
             print OWN_ID
             if message != '':
-                print message
+                # print message
+                pass
             #if  "MASTER_START" in message:
             if "START" in message:
                 state = "NOT_IDLE"
@@ -49,26 +66,37 @@ try:
             if time.time() - last_dist_check > 0.2:
                 last_dist_check = time.time()
                 distance = pi2go.getDistance()
+
+                # print state_list
             irCentre = pi2go.irCentre()
+
             if irCentre or distance < MIN_DIST:
-                state_list[OWN_ID] = STOPPED
+                state_list[OWN_ID] = OBSTACLE
                 #print "After setting it: own state: " + str(state_list[OWN_ID])
                 #print "irCentre: " + str(irCentre)
             else:
-                state_list[OWN_ID] = RUNNING
+                state_list[OWN_ID] = CLEAR
+            # print "P : ", prev_state_list[OWN_ID]
+            # print "C : ", state_list[OWN_ID]
+            if prev_state_list[OWN_ID] != state_list[OWN_ID]:
+                print "Sending state : ", str(state_list[OWN_ID])
+                for x in range(10):
+                    communication.send_broadcast_message(PORT, str(state_list[OWN_ID]))
+
             if distance < SLOW_DIST:
                 slow = True
             else:
                 slow = False
-            print "BEFORE RECEIVING: " + str(state_list)
-            ##### RECEIVE
+
+            # print "BEFORE RECEIVING: " + str(state_list)
+            #### RECEIVE
             message_list = communication.receive_message_list(sock)
-            state_list = communication.update_state_list(state_list, message_list, OWN_ID)
+            update_state_list()
             
             ##### SEND
-            print "    AFTER RECEIVING: " + str(state_list)
+            # print "    AFTER RECEIVING: " + str(state_list)
             
-            ##### SET SUB_STATE            :
+            ##### SET SUB_STATE
             if all(state_list):
                 if slow:                   
                     sub_state_motor = "SLOW"
@@ -76,30 +104,29 @@ try:
                     sub_state_motor = "RUN"
             else:
                 sub_state_motor = "STOP"
-            #print "After receiving: own state: " + str(state_list[OWN_ID]) + " " + sub_state_motor
+            # print "After receiving: own state: " + str(state_list[OWN_ID]) + " " + sub_state_motor
             ##### SUB_STATE: MOTOR
             if prev_sub_state_motor == sub_state_motor:
                 #print "I'll pass.."
                 pass
             elif sub_state_motor == "RUN":
-                #print sub_state_motor
+                # print sub_state_motor
                 pi2go.turnForward(L_SPEED_RUN, R_SPEED_RUN)
                 pi2go.setAllLEDs(LED_OFF, LED_ON, LED_OFF)
             elif sub_state_motor == "SLOW":
-                #print sub_state_motor
+                # print sub_state_motor
                 pi2go.turnForward(L_SPEED_SLOW, R_SPEED_SLOW)
                 pi2go.setAllLEDs(LED_ON, LED_ON, LED_OFF)
             elif sub_state_motor == "STOP":
-                #print sub_state_motor
+                # print sub_state_motor
                 pi2go.stop()
                 pi2go.setAllLEDs(LED_ON, LED_OFF, LED_OFF)
             else:
-                print "I am so confused...."                
-            if prev_sub_state_motor != sub_state_motor:
-                for x in range (10):
-                    communication.send_broadcast_message(PORT, sub_state_motor)
-                
+                print "I am so confused...."
+
             prev_sub_state_motor = sub_state_motor
+            prev_state_list = state_list[:]
+
                 
 except object as p:
     print p            
