b1f289db6708aefde34b1cd7105003c14fcf9155
Christopher.Lehmann <christopher.lehmann@mailbox.tu-dresden.de>
2015-10-29 15:53:20 +0100

    old version

--- push_file.sh
+++ push_file.sh
@@ -1,18 +1,18 @@
-##!/bin/bash
+#!/bin/bash
 # send files to all robots
 
 FILE=$1
-IP_START=101
-ROBOT_COUNT=2
+IP_START=100
+ROBOT_COUNT=10
 IP_END=$(($IP_START+$ROBOT_COUNT))
 echo "Transmitting $FILE to Robots in IP-Range from 192.168.178.$IP_START to 192.168.178.$IP_END"
 for ip_counter in $(seq $IP_START $IP_END)
 do
-	# ping  192.168.178.$ip_counter  # 2>/dev/null 1>/dev/null
-	if [ 0 = 0 ]
+	ping -c 1  192.168.178.$ip_counter -w1  2>/dev/null 1>/dev/null
+	if [ "$?" = 0 ]
 	then
 		echo "Connection to 192.168.178.$ip_counter successfull!"
-		scp  $FILE pi@192.168.178.$ip_counter:./pi2go >> filename
+		scp -q $FILE pi@192.168.178.$ip_counter:./pi2go >> filename
 		
 	else
 		echo "failed: 192.168.178.$ip_counter"
