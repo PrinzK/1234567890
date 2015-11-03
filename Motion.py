709cb184a872a98915022c2c0e82374f2307a38b
PrinzK <david_nophut@hotmail.com>
2015-11-03 11:10:40 +0100

    finished distance_control in Motion library

--- Motion.py
+++ Motion.py
@@ -1,7 +1,17 @@
-def distance_control(speed_slow = 0, SPEED_RUN, DIST_REF, distance, KP, SPEED_CONTROL_MAX):
-    SPEED_SLOW = SPEED_RUN - (DIST_REF-DISTANCE) * KP
+import constants
+
+def distance_control(distance, speed_slow, SPEED_RUN = 0, DIST_REF, KP, SPEED_CONTROL_MAX, SPEED_CONTROL_MIN):
+    if speed_slow == 0:
+        SPEED_RUN = constants.SPEED_RUN
+        DIST_REF = constants.SPEED_RUN
+        KP = constants.KP
+        SPEED_CONTROL_MAX = constants.SPEED_CONTROL_MAX
+        SPEED_CONTROL_MIN = constants.SPEED_CONTROL_MIN
+    speed_slow = SPEED_RUN - (DIST_REF-DISTANCE) * KP
                 # Controlllimits
-                if SPEED_SLOW > SPEED_CONTROL_MAX:
-                    SPEED_SLOW = SPEED_CONTROL_MAX
-                elif SPEED_SLOW < SPEED_CONTROL_MIN:
-                    SPEED_SLOW = SPEED_CONTROL_MIN
+    if speed_slow > SPEED_CONTROL_MAX:
+        speed_slow = SPEED_CONTROL_MAX
+    elif speed_slow < SPEED_CONTROL_MIN:
+        speed_slow = SPEED_CONTROL_MIN
+    return speed_slow
+    
